7ce3f5ebe0044a2a1d2aa4cc5aa81c40b2a411f1
JonasReynders434 <jonas.reynders@student.kuleuven.be>
2026-07-13 13:37:17 +0000

    paddle movement

--- src/pong_logic.v
+++ src/pong_logic.v
@@ -31,6 +31,15 @@ module pong (clk, reset, l_paddle_up, l_paddle_down, r_paddle_up, r_paddle_down,
     reg ball_dir_x = 0; // 1 => to left, 0 => to right
     reg ball_dir_y = 0; // 1 => to top, 0 => to bottom
 
+    wire [9:0] next_y_up = ball_y - BALL_SPEED;         // what next ball_y would be without boundries
+    wire [9:0] next_y_down = ball_y + X + BALL_SPEED;
+
+    wire top_hit = (ball_y <= BALL_SPEED) && (ball_dir_y == 1);
+    wire bottom_hit = (next_y_down >= V_DISPLAY) && (ball_dir_y == 0);
+
+    wire l_paddle_hit = ball_dir_x == 1 && (ball_x <= L_PADDLE_X + X + BALL_SPEED && ball_x + X >= L_PADDLE_X) && (ball_y + X >= l_paddle_y && ball_y < l_paddle_y + PADDLE_HEIGHT);
+    wire r_paddle_hit = ball_dir_x == 0 && (ball_x <= R_PADDLE_X + X && ball_x + X + BALL_SPEED >= R_PADDLE_X) && (ball_y + X >= r_paddle_y && ball_y < r_paddle_y + PADDLE_HEIGHT);
+
 
     always @(posedge clk) begin
 
@@ -49,26 +58,23 @@ module pong (clk, reset, l_paddle_up, l_paddle_down, r_paddle_up, r_paddle_down,
 
         end else begin
 
-            // ball position update
-            if (ball_dir_x == 0) begin
-                ball_x <= ball_x + BALL_SPEED;
-            end else begin
-                ball_x <= ball_x - BALL_SPEED;
-            end
-
-            if (ball_dir_y == 0) begin
-                ball_y <= ball_y + BALL_SPEED;
-            end else begin
-                ball_y <= ball_y - BALL_SPEED;
-            end
-
             // top border collision
-            if (ball_y <= BALL_SPEED) begin
+            if (top_hit) begin
                 ball_dir_y <= 0;
+                ball_y <= BALL_SPEED - ball_y;      // rebounce of border
+
             // bottom border collision
-            end else if (ball_y + X + BALL_SPEED >= V_DISPLAY) begin
+            end else if (bottom_hit) begin
                 ball_dir_y <= 1;
-            end 
+                ball_y <= (V_DISPLAY - X) - (next_y_down - V_DISPLAY);
+            
+            // NO top/bottom collision
+            end else if (ball_dir_y == 1) begin
+                ball_y <= ball_y - BALL_SPEED;
+
+            end else begin
+                ball_y <= ball_y + BALL_SPEED;
+            end
 
             // left border collision
             if (ball_x <= BALL_SPEED) begin        
@@ -105,12 +111,57 @@ module pong (clk, reset, l_paddle_up, l_paddle_down, r_paddle_up, r_paddle_down,
                 end
 
             // left paddle collision
-            end else if (ball_x == L_PADDLE_X + X && (ball_y + X >= l_paddle_y && ball_y < l_paddle_y + PADDLE_HEIGHT))  begin
+            end else if (l_paddle_hit)  begin
                     ball_dir_x <= 0;
+                    ball_x <= L_PADDLE_X + X;
+
             // right paddle collision
-            end else if (ball_x == R_PADDLE_X && (ball_y + X >= r_paddle_y && ball_y < r_paddle_y + PADDLE_HEIGHT)) begin
+            end else if (r_paddle_hit) begin
                 ball_dir_x <= 1;
+                ball_x <= R_PADDLE_X - X;
+
+            // NO paddle collision
+            end else if (ball_dir_x == 0) begin
+                ball_x <= ball_x + BALL_SPEED;
+            end else begin
+                ball_x <= ball_x - BALL_SPEED;
             end
+
+
+            // left paddle movement
+            if (l_paddle_down && !l_paddle_up) begin
+                if (l_paddle_y + PADDLE_HEIGHT + PADDLE_SPEED <= V_DISPLAY) begin
+                    l_paddle_y <= l_paddle_y + PADDLE_SPEED;
+                end else begin
+                    l_paddle_y <= V_DISPLAY - PADDLE_HEIGHT; // paddle almost at bottom ==> set paddle at bottom
+                end
+
+            end else if (!l_paddle_down && l_paddle_up) begin
+                if (l_paddle_y >= PADDLE_SPEED) begin
+                    l_paddle_y <= l_paddle_y - PADDLE_SPEED;
+                end else begin
+                    l_paddle_y <= 0;
+                end
+            end
+
+            // right paddle movement
+            if (r_paddle_down && !r_paddle_up) begin
+                if (r_paddle_y + PADDLE_HEIGHT + PADDLE_SPEED <= V_DISPLAY) begin // if not beyond the border
+                    r_paddle_y <= r_paddle_y + PADDLE_SPEED;
+                end else begin
+                    r_paddle_y <= V_DISPLAY - PADDLE_HEIGHT; // set against bottom border
+                end
+
+            end else if (!r_paddle_down && r_paddle_up) begin
+                if (r_paddle_y >= PADDLE_SPEED) begin
+                    r_paddle_y <= r_paddle_y - PADDLE_SPEED;
+                end else begin
+                    r_paddle_y <= 0;
+                end
+            end
+
+
+
         end
     end
 endmodule
